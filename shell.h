05c7faa516ce9d81ee2a1cc0f8c0f3065ea8765b
Eziknmad <galarzaarzon@gmail.com>
2026-01-05 15:54:54 -0500

    fist hsh

--- shell.h
+++ shell.h
@@ -4,10 +4,6 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
-extern char **environ;
 
 /**
  * shell_loop - main shell loop
@@ -16,20 +12,4 @@ extern char **environ;
  */
 void shell_loop(void);
 
-/**
- * print_prompt - displays shell prompt
- *
- * Return: void
- */
-void print_prompt(void);
-
-/**
- * execute_command - executes a single command
- * @line: command entered by user
- *
- * Return: void
- */
-void execute_command(char *line);
-
 #endif /* SIMPLE_SHELL_H */
-
