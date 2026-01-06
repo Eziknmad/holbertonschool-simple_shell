efa0da0bed5b2f3abeeacb7510b914d6638afb15
Eziknmad <galarzaarzon@gmail.com>
2026-01-05 23:20:00 -0400

    made some edits acording to betty

--- path.c
+++ path.c
@@ -1,47 +1,66 @@
 #include "shell.h"
 
 /**
- * find_command - finds executable in PATH
+ * _getenv - gets the value of an environment variable
+ * @name: variable name
+ *
+ * Return: pointer to value or NULL
+ */
+char *_getenv(const char *name)
+{
+	int i, j;
+
+	if (!name || !environ)
+		return (NULL);
+
+	for (i = 0; environ[i]; i++)
+	{
+		for (j = 0; name[j] && environ[i][j] && name[j] == environ[i][j]; j++)
+			;
+
+		if (name[j] == '\0' && environ[i][j] == '=')
+			return (&environ[i][j + 1]);
+	}
+	return (NULL);
+}
+
+/**
+ * find_command - finds command in PATH or returns NULL if not found
  * @command: command name
  *
- * Return: full path or NULL
+ * Return: full path string (malloc'd) or NULL if not found
  */
 char *find_command(char *command)
 {
-char *path, *copy, *dir, *full;
-int len;
+	char *path, *path_copy, *dir;
+	char full_path[1024];
 
-if (command[0] == '/' && access(command, X_OK) == 0)
-return (strdup(command));
+	if (command[0] == '/')
+	{
+		if (access(command, X_OK) == 0)
+			return (strdup(command));
+		return (NULL);
+	}
 
-path = _getenv("PATH");
-if (!path)
-return (NULL);
+	path = _getenv("PATH");
+	if (!path)
+		return (NULL);
 
-copy = strdup(path);
-dir = strtok(copy, ":");
+	path_copy = strdup(path);
+	if (!path_copy)
+		return (NULL);
 
-while (dir)
-{
-len = strlen(dir) + strlen(command) + 2;
-full = malloc(len);
-if (!full)
-break;
-
-strcpy(full, dir);
-strcat(full, "/");
-strcat(full, command);
-
-if (access(full, X_OK) == 0)
-{
-free(copy);
-return (full);
-}
-
-free(full);
-dir = strtok(NULL, ":");
-}
-
-free(copy);
-return (NULL);
+	dir = strtok(path_copy, ":");
+	while (dir)
+	{
+		snprintf(full_path, sizeof(full_path), "%s/%s", dir, command);
+		if (access(full_path, X_OK) == 0)
+		{
+			free(path_copy);
+			return (strdup(full_path));
+		}
+		dir = strtok(NULL, ":");
+	}
+	free(path_copy);
+	return (NULL);
 }
--- utils.c
+++ utils.c
@@ -1,23 +1,54 @@
 #include "shell.h"
 
 /**
- * trim_whitespace - trims spaces
- * @str: string
+ * trim_whitespace - removes leading and trailing whitespace from a string
+ * @str: string to trim
  *
  * Return: void
  */
 void trim_whitespace(char *str)
 {
-int i = 0, j = 0;
+	int start = 0, end = strlen(str) - 1;
+	int i = 0;
 
-while (str[i] == ' ' || str[i] == '\t')
-i++;
+	while (str[start] == ' ' || str[start] == '\t')
+		start++;
+	while (end >= start && (str[end] == ' ' || str[end] == '\t'))
+		end--;
 
-while (str[i])
-str[j++] = str[i++];
+	while (start <= end)
+		str[i++] = str[start++];
+	str[i] = '\0';
+}
 
-while (j > 0 && (str[j - 1] == ' ' || str[j - 1] == '\t'))
-j--;
+/**
+ * split_line - splits a line into arguments array by whitespace
+ * @line: input line
+ *
+ * Return: NULL-terminated array of strings (needs free), or NULL on failure
+ */
+char **split_line(char *line)
+{
+	int bufsize = 64, position = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token;
 
-str[j] = '\0';
+	if (!tokens)
+		return (NULL);
+
+	token = strtok(line, " \t");
+	while (token != NULL)
+	{
+		tokens[position++] = token;
+		if (position >= bufsize)
+		{
+			bufsize += 64;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+				return (NULL);
+		}
+		token = strtok(NULL, " \t");
+	}
+	tokens[position] = NULL;
+	return (tokens);
 }
